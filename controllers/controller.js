22a49e0df64fb3bd7a7426cab5284c8c705d8a03
ebin <ebinyesudas234@gmail.com>
2023-01-19 23:23:25 +0530

    final commit

--- controllers/controller.js
+++ controllers/controller.js
@@ -7,23 +7,16 @@ const octokit = new Octokit({
 
 module.exports={
     getUserDetails : async(req,res)=>{
-        console.log(process.env.ACCESS_TOKEN)
         try{
         const userData = await octokit.request('GET /users/{username}', {
             username: req.params.username  
           })
-          
-          
-        console.log("userdata",userData)
         return res.status(userData.status).json(userData)
         }catch(err){
-          console.log("error",err)
-          // return res.json(err.message).status(404)
           res.status(404).json({data:"Invalid Github Username",status:404});
         }
     },
     getUserRepositories : async(req,res)=>{
-      console.log(req.params)
       try{
         const repositories = await octokit.request('GET /users/{username}/repos{?type,updated,direction,per_page,page}', {
             username: req.params.username,
@@ -31,12 +24,9 @@ module.exports={
             per_page: '10',
             page:req.params.page
           })
-          console.log("repo : ",repositories)
           return res.status(repositories.status).json(repositories)
       }catch(err){
-        // console.log(err)
         res.status(404).json({data:"Invalid Github Username",status:404});
-        
       }
     },    
 }
